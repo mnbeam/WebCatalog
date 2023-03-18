67a8db85c20d910673958e31be86c82c0c13c805
OorzhakAV <oorzhak.iv@gmail.com>
2023-03-19 00:51:08 +0700

    Add JWT authoriztion to swagger

--- WebCatalog.Api/Program.cs
+++ WebCatalog.Api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using WebCatalog.Api.Middlewares;
 using WebCatalog.Api.UserAccessor;
 using WebCatalog.Domain.Entities;
@@ -23,7 +24,30 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddTransient<IUserAccessor, UserAccessor>();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "JWTToken_Auth_API", Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer tokenValue\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.AddIdentity<AppUser, IdentityRole<int>>(options =>
     {
